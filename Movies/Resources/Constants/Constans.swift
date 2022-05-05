9fd3050cd77a2b62aa72bbaef6972840e69f3cf5
Mert Karadeniz <mertrizakaradeniz@gmail.com>
2022-05-05 15:45:46 +0300

    Constants updated

--- Movies/Resources/Constants/Constans.swift
+++ Movies/Resources/Constants/Constans.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Constants {
-    static let API_KEY = "dfdb816884d9a69e72596ce6f33e8952"
+    static let API_KEY = "YOUR_API_KEY"
     static let BASE_URL = "https://api.themoviedb.org/3/"
     static let IMAGE_BASE_URL_WIDTH_92 = "https://image.tmdb.org/t/p/w92"
     static let IMAGE_BASE_URL_WIDTH_500 = "https://image.tmdb.org/t/p/w500"
